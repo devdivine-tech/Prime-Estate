a16d6a6b06d5024581e8f806dac58b8d8e34fbb8
Dev Divine 8 <devdivine8@gmail.com>
2025-02-16 01:57:15 +0100

    INCOMPLETE LANDING PAGE
     Am about finishing thw landing page, i just need to complete the cta section and the footer section, thats all

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,9 @@
+let mobileMenu = document.querySelector('.mobile-menu');
+
 // MAIN MENU 
 const menuFuction = () => {
-  let menuIcon  = document.querySelector(".menu-icon");
-  let menu = document.querySelector(".menu");
+  let menuIcon  = mobileMenu.querySelector(".menu-icon");
+  let menu = mobileMenu.querySelector(".menu");
   const openMenu = () => {
     menu.classList.toggle("hidden");
     if (!menu.classList.contains("hidden")) {
@@ -12,14 +14,15 @@ const menuFuction = () => {
   }
   
   menuIcon.addEventListener("click", openMenu);
-}
-menuFuction();
+};
 
-// LIST MENU
+// LIST MENU || MOBILE VIEWS
 const listMenu = () => {
-  let dropListMenu = document.querySelectorAll(".drop-down");
-  let opt1 = document.querySelector(".opt1")
-  let opt2 = document.querySelector(".opt2")
+  let dropListMenu = mobileMenu.querySelectorAll(".drop-down");
+  let opt1 = mobileMenu.querySelector(".opt1");
+  let opt2 = mobileMenu.querySelector(".opt2");
+  let opt3 = mobileMenu.querySelector(".opt3");
+  
   console.log(opt1.classList);
   
   const openListMenu1 = () => {
@@ -37,6 +40,7 @@ const listMenu = () => {
       }
     });
   }
+  
   const openListMenu2 = () => {
     opt2.classList.toggle("hidden");
     
@@ -53,18 +57,99 @@ const listMenu = () => {
     });
   }
   
+  const openListMenu3 = () => {
+    opt3.classList.toggle("hidden");
+    
+    dropListMenu.forEach((list3, index) => {
+      if (index === 1) {
+        if (!opt3.classList.contains("hidden")) {
+        list3.classList.remove("fa-chevron-down");
+        list3.classList.add("fa-chevron-up");
+      } else {
+        list3.classList.add("fa-chevron-down");
+        list3.classList.remove("fa-chevron-up");
+      };
+      }
+    });
+  }
+  
   dropListMenu.forEach((dropMenu, index) => {
     if (index === 0) {
       dropMenu.addEventListener("click", openListMenu1);
-    } else {
+    } else if (index === 1) {
       dropMenu.addEventListener("click", openListMenu2);
+    } else {
+      dropMenu.addEventListener("click", openListMenu3);
     }
   });
-}
-listMenu();
+};
+
+// LIST MENU || DESKTOP VIEWS
+const listOpt = () => {
+  let ul = document.querySelectorAll(".menu-list ul");
+  let a = document.querySelectorAll(".menu-list .list-opt");
+  
+  const toggleSelection = () => {
+    ul.forEach((ul, index) => {
+      if (index === 0) {
+        ul.classList.toggle("hidden");
+      }
+    });
+  };
+  const toggleSelection1 = () => {
+    ul.forEach((ul, index) => {
+     if (index === 1) {
+        ul.classList.toggle("hidden");
+     }
+    });
+  };
+  
+  const toggleSelection2 = () => {
+    ul.forEach((ul, index) => {
+     if (index === 2) {
+        ul.classList.toggle("hidden");
+     }
+    });
+  };
+  
+  a.forEach((a, index) => {
+    if (index === 0) {
+      a.addEventListener("mouseover", toggleSelection);
+    } else if (index === 1) {
+      a.addEventListener("mouseover", toggleSelection1);
+    } else {
+      a.addEventListener("mouseover", toggleSelection2);
+    }
+  });
+};
 
 
+
+
+
+// MOBILE FUCTIONS ALLOWED TO RUB
+const mobileFuction = () => {
+  if (window.innerWidth <= 985) {
+    menuFuction();
+    listMenu();
+  };
+};
+mobileFuction();
+
+// DESKTOP FUNCTIONS ALLOWED TO RUN
+const desktopFuction = () => {
+  let menu = mobileMenu.querySelector(".menu");
+  if (window.innerWidth >= 985) {
+    menu.classList.remove("hidden");
+    listOpt();
+  };
+};
+desktopFuction();
+
+
+
+// ANIMATE ON SCROLL SCRIPT
 AOS.init({
-  delay: 300,
+  delay: 200,
   duration: 800,
 });
