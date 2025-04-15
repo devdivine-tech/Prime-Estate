20acf7113824d111ee17c63474f666390434aa13
dev-divine7 <devdivine8@gmail.com>
2025-04-15 22:19:04 +0100

    New Changes

--- js/desktop.js
+++ js/desktop.js
@@ -1,9 +1,4 @@
 // DESKTOP FUNCTIONS ALLOWED TO RUN
 const desktopFuction = () => {
-  let menu = document.querySelector(".menu");
-  if (window.innerWidth >= 985) {
-    menu.classList.remove("hidden");
-    listOpt();
-  };
 };
 desktopFuction();
--- js/mobile.js
+++ js/mobile.js
@@ -1,7 +1,6 @@
 // MOBILE FUCTIONS ALLOWED TO RUB
 const mobileFuction = () => {
   if (window.innerWidth <= 985) {
-    listMenu();
     menuToggle();
   };
 };
